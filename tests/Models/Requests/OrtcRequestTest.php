3fd55042ed9b102aafc06e99a671da73ba563dbf
Diego Cortés <diegocortes@blenderdeluxe.com>
2017-12-11 02:59:56 -0300

    Update OrtcRequestTest.php

--- tests/Models/Requests/OrtcRequestTest.php
+++ tests/Models/Requests/OrtcRequestTest.php
@@ -3,14 +3,14 @@
 namespace Tests\Models\Requests;
 
 use Mockery as m;
-use Nikapps\OrtcPhp\Configs\OrtcConfig;
+use Blenderdeluxe\OrtcPhp\Configs\OrtcConfig;
 use Tests\TestCase;
 
 class OrtcRequestTest extends TestCase
 {
     public function testImplementsDefault()
     {
-        $request = m::mock('Nikapps\OrtcPhp\Models\Requests\OrtcRequest[]');
+        $request = m::mock('Blenderdeluxe\OrtcPhp\Models\Requests\OrtcRequest[]');
         $ortcConfig = new OrtcConfig();
 
         $this->assertEquals($request, $request->setOrtcConfig($ortcConfig));
