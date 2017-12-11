370455fe5a6b010a77ec94b40aa34f197705c7c8
Diego Cortés <diegocortes@blenderdeluxe.com>
2017-12-11 02:59:16 -0300

    Update BalancerUrlRequestTest.php

--- tests/Models/Requests/BalancerUrlRequestTest.php
+++ tests/Models/Requests/BalancerUrlRequestTest.php
@@ -16,11 +16,11 @@ class BalancerUrlRequestTest extends TestCase
         $request = new BalancerUrlRequest();
         $request->setOrtcConfig($ortcConfig);
 
-        $this->assertInstanceOf('Nikapps\OrtcPhp\Models\Requests\OrtcRequest', $request);
+        $this->assertInstanceOf('Blenderdeluxe\OrtcPhp\Models\Requests\OrtcRequest', $request);
         $this->assertEquals('https://ortc-developers.realtime.co/server/2.1?appkey=FOoB1r', $request->getUrlPath());
         $this->assertFalse($request->isPost());
         $this->assertEquals([], $request->getPostData());
         $this->assertTrue($request->isUrlAbsolute());
-        $this->assertInstanceOf('Nikapps\OrtcPhp\Handlers\BalancerUrlResponseHandler', $request->getResponseHandler());
+        $this->assertInstanceOf('Blenderdeluxe\OrtcPhp\Handlers\BalancerUrlResponseHandler', $request->getResponseHandler());
     }
 }
